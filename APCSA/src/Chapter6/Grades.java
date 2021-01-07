ae082517aed57cafc445d027956499878e9ce674
Vignesh300405 <vigsk17@gmail.com>
2021-01-07 17:53:16 -0500

    Corrected Labs 6.1-6.2 (6.3 is already correct)

--- APCSA/src/Chapter6/Grades.java
+++ APCSA/src/Chapter6/Grades.java
@@ -2,6 +2,18 @@ package Chapter6;
 
 import java.util.Scanner;
 
+
+//****************************************************************
+// Grades.java                            Project or Lab Name: Lab Exercises 6.2
+//
+// Author: Vignesh Suresh Kumar         Date: January 8, 2021
+//
+// This program is meant to create a means for a teacher to grade multiple quizzes,
+// allowing for the teacher to input the amount of questions, the answer key, and a 
+// loop for inputting student answers and receiving a percent and amount right out the
+// total question amount.
+//****************************************************************
+
 public class Grades {
 
     public static void main(String[] args) {
@@ -11,16 +23,17 @@ public class Grades {
         int percentCorrect;
         char loop = 'y';
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("How many questions are on the quiz? ");
+        answerKey = new int[scan.nextInt()];
+
+        System.out.print("Input the correct answers: ");
+        for (int i = 0; i < answerKey.length; i++) {
+            answerKey[i] = scan.nextInt();
+        }
+
         while(loop == 'y') {
-            Scanner scan = new Scanner(System.in);
-
-            System.out.print("How many questions are on the quiz? ");
-            answerKey = new int[scan.nextInt()];
-
-            System.out.print("Input the correct answers: ");
-            for (int i = 0; i < answerKey.length; i++) {
-                answerKey[i] = scan.nextInt();
-            }
 
             System.out.print("Input the answers from a student's quiz: ");
             correct = 0;
--- APCSA/src/Chapter6/Sales.java
+++ APCSA/src/Chapter6/Sales.java
@@ -1,56 +1,57 @@
 package Chapter6;
 
 
-import java.lang.reflect.Array;
+//****************************************************************
+// Sales.java                            Project or Lab Name: Lab Exercises 6.1
+//
+// Author: Vignesh Suresh Kumar         Date: January 8, 2021
+//
+// Determine various statistics based upon a number of salespeople and the
+// amount of sales for each salesperson.
+//****************************************************************
+
 import java.util.Scanner;
 
 public class Sales {
-
+    
     public static void main(String[] args) {
-
-        final int SALESPEOPLE = 5;
-        int[] sales;
         int sum;
         int maxSale = Integer.MIN_VALUE;
         int minSale = Integer.MAX_VALUE;
 
-
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("How many salespeople? ");
-        sales = new int[scan.nextInt() + 1];
+        System.out.println("Number of salespeople: ");
+        int[] sales = new int[scan.nextInt() + 1];
 
-        for(int i = 1; i < sales.length; i++) {
+        
+	    for (int i=1; i<sales.length; i++)
+	    {
+            if(i == 1) {sales[0] = 1;}
+		    System.out.print("Enter sales for salesperson " + i + ": ");
+		    sales[i] = scan.nextInt();
+	    }
 
-            System.out.println("Enter sales for salesperson " + i + ": ");
-            sales[i] = scan.nextInt();
-
-        }
-
-
-        System.out.println("\nSalesperson  Sales");
-        System.out.println("--------------------");
+	    System.out.println("\nSalesperson   Sales");
+	    System.out.println("--------------------");
         sum = 0;
-        for(int i = 1; i < sales.length; i++) {
+        
 
-            System.out.println("     " + i + "        " + sales[i]);
-            sum += sales[i];
-
-        }
-        System.out.println("\nTotal sales: " + sum);
-        System.out.println("Average sale: " + (sum / 5));
-
-        for (int sale : sales) {
-
-            if (maxSale < sale) {
-                maxSale = sale;
+	    for (int i=1; i<sales.length; i++)
+	    {
+            if(maxSale < sales[i]) {
+                maxSale = sales[i];
             }
-
-            if(minSale > sale) {
-                minSale = sale;
+            if(minSale > sales[i]) {
+                minSale = sales[i];
             }
-
+	    	System.out.println("     " + i + "         " + sales[i]);
+		    sum += sales[i];
         }
+        
+
+        System.out.println("\nTotal sales: " + sum); 
+        System.out.println("Average sale: " + (sum / (sales.length - 1)));
         System.out.println("Maximum sale: " + maxSale);
         System.out.println("Minimum sale: " + minSale);
 
@@ -63,8 +64,7 @@ public class Sales {
                 System.out.println("User " + i + " exceeded the value with " + sales[i]);
             }
 
-        }
-
+        } 
     }
 
 }
