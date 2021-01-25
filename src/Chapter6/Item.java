557e4927c0903459a6ade33046169a3e3acd0291
Vignesh300405 <vigneshsureshkumar17@gmail.com>
2021-01-25 18:40:52 -0500

    a1

--- src/Chapter6/Item.java
+++ src/Chapter6/Item.java
@@ -1,33 +1,20 @@
 package Chapter6;
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
 import java.text.DecimalFormat;
 
-=======
->>>>>>> test
->>>>>>> Stashed changes
-=======
 import java.text.DecimalFormat;
 
->>>>>>> Stashed changes
 public class Item {
 
     private String name;
     private double price;
     private int quantity;
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     public Item(String aName, double aPrice, int aQuantity) {
 
         name = aName;
         price = aPrice;
         quantity = aQuantity;
-=======
-<<<<<<< HEAD
     DecimalFormat df = new DecimalFormat("#.00");
 
     public Item(String itemName, double itemPrice, int itemQuantity) {
@@ -35,15 +22,6 @@ public class Item {
         name = itemName;
         price = itemPrice;
         quantity = itemQuantity;
-=======
-    public Item(String aName, double aPrice, int aQuantity) {
-
-        name = aName;
-        price = aPrice;
-        quantity = aQuantity;
->>>>>>> test
->>>>>>> Stashed changes
-=======
     DecimalFormat df = new DecimalFormat("#.00");
 
     public Item(String itemName, double itemPrice, int itemQuantity) {
@@ -51,26 +29,15 @@ public class Item {
         name = itemName;
         price = itemPrice;
         quantity = itemQuantity;
->>>>>>> Stashed changes
 
     }
 
     public String toString() {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         return "Item Name: " + name + ", Item Price: $" + price + ", Item Quantity: " + quantity;
-=======
-<<<<<<< HEAD
         // return name + "\t\t" + df.format(price) + "\t\t" + quantity + "\t\t" + df.format(price * quantity);
         return String.format("%-17s %-17s %-17s %-17s", name, df.format(price), quantity, df.format(price * quantity));
-=======
-        return "Item Name: " + name + ", Item Price: $" + price + ", Item Quantity: " + quantity;
->>>>>>> test
->>>>>>> Stashed changes
-=======
         // return name + "\t\t" + df.format(price) + "\t\t" + quantity + "\t\t" + df.format(price * quantity);
         return String.format("%-17s %-17s %-17s %-17s", name, df.format(price), quantity, df.format(price * quantity));
->>>>>>> Stashed changes
     }
 
     public String getName() {
@@ -85,18 +52,7 @@ public class Item {
         return quantity;
     }
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
 }
-=======
-<<<<<<< HEAD
 }
-=======
-
 }
->>>>>>> test
->>>>>>> Stashed changes
-=======
-}
->>>>>>> Stashed changes
--- src/Chapter6/Lab6/Shop.java
+++ src/Chapter6/Lab6/Shop.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 public class Shop {
 
     public static void main (String[] args) {
+<<<<<<< Updated upstream
 
         Item item;
         String itemName;
@@ -28,26 +29,60 @@ public class Shop {
 	    {
 		System.out.print ("Enter the name of the item: "); 
 		itemName = scan.nextLine();
+=======
 
-		System.out.print ("Enter the unit price: ");
-		itemPrice = scan.nextDouble();
+        Item item;
+        String itemName;
+        double itemPrice;
+        int quantity;
 
-		System.out.print ("Enter the quantity: ");
-		quantity = scan.nextInt();
+        ArrayList<Item> cart = new ArrayList<>();
+        double totalPrice;
+        DecimalFormat df = new DecimalFormat("#.00");
 
-        // *** create a new item and add it to the cart
-        
-        Item newItem = new Item(itemName, itemPrice, quantity);
-        cart.add(newItem);
+        Scanner scan = new Scanner(System.in);
 
+        String keepShopping = "y";
 
-        // *** print the contents of the cart object using println
-        
-        System.out.println(cart);
+        do {
+            System.out.print ("Enter the name of the item: ");
+            itemName = scan.nextLine();
 
+            System.out.print ("Enter the unit price: ");
+            itemPrice = scan.nextDouble();
+
+            System.out.print ("Enter the quantity: ");
+            quantity = scan.nextInt();
+
+            // *** create a new item and add it to the cart
+>>>>>>> Stashed changes
+
+            item = new Item(itemName, itemPrice, quantity);
+            cart.add(item);
+
+            // *** print the contents of the cart using println
+
+            System.out.println();
+            System.out.println("Cart Items:");
+            System.out.println("---------------");
+            totalPrice = 0;
+            for(Item aItem : cart) {
+
+                System.out.println(aItem.getName());
+                totalPrice += aItem.getQuantity() * aItem.getPrice();
+
+            }
+            System.out.println("Total Price: $" + df.format(totalPrice));
+
+            System.out.println();
+            System.out.println("Continue shopping (y/n)?");
+            scan.nextLine();
+            keepShopping = scan.nextLine();
+            System.out.println();
 
         } while (keepShopping.equals("y"));
         System.out.println("Final Price: $" + df.format(totalPrice));
+<<<<<<< Updated upstream
     public static void main (String[] args)
     {
 =======
@@ -101,6 +136,8 @@ public class Shop {
 =======
         } while (keepShopping.equals("y"));
         System.out.println("Final Price: $" + df.format(totalPrice));
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 
     }
